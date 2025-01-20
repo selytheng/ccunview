8d06066f8ee6e5b9fbfb60706c93885c6f9ce26b
POMMOUYLANG <mouylangpom@gamail.com>
2025-01-20 21:46:39 +0700

    HomePage-20-01-25

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './Pages/User/Home.tsx';
-import Courses from './Pages/User/Courses.tsx';
-import Login from './Pages/Login.tsx';
-import About from './Pages/User/About.tsx';
-import Contact from './Pages/User/Contact.tsx';
-import AdminDashboard from './Pages/Admin/AdminDashboard';
-import AdminCourse from './Pages/Admin/Course/AdminCourse';
-import CourseEdit from './Pages/Admin/Course/CourseEdit';
-import CourseCreate from './Pages/Admin/Course/CourseCreate';
-import AdminTraining from './Pages/Admin/AdminTraining';
-import AdminWorkshop from './Pages/Admin/AdminWorkshop';
-import AdminEvent from './Pages/Admin/AdminEvent';
-import AdminPartner from './Pages/Admin/Partner/AdminPartner.tsx';
-import Feedback from './Pages/Admin/Feedback';
-import CourseDetail from './Pages/Admin/Course/CourseDetail';
-import AdminRoute from '../src/components/AdminRoute'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Home from "./Pages/Home";
-import Courses from "./Pages/Courses";
+import Home from "./Pages/User/Home";
+import Courses from "./Pages/User/Courses";
 import Login from "./Pages/Login";
-import About from "./Pages/About";
-import Contact from "./Pages/Contact";
+import About from "./Pages/User/About";
+import Contact from "./Pages/User/Contact";
 import AdminDashboard from "./Pages/Admin/AdminDashboard";
 import AdminCourse from "./Pages/Admin/Course/AdminCourse";
 import CourseEdit from "./Pages/Admin/Course/CourseEdit";
@@ -35,134 +17,132 @@ import Feedback from "./Pages/Admin/Feedback";
 import CourseDetail from "./Pages/Admin/Course/CourseDetail";
 import AdminRoute from "../src/components/AdminRoute";
 import AdminProfile from "./Pages/Admin/AdminProfile";
-import News from "./Pages/News.tsx";
+import News from "./Pages/User/News.tsx";
 import AdminMajor from "./Pages/Admin/Major/AdminMajor";
 import Users from "./Pages/SuperAdmin/Users";
-import News from "./Pages/User/News.tsx";
-import AdminMajor from './Pages/Admin/Major/AdminMajor.tsx';
 
 const App = () => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/user/home" element={<Home />} />
-          <Route path="/user/news" element={<News />} />
-          <Route path="/user/course" element={<Courses />} />
-          <Route path="/user/about" element={<About />} />
-          <Route path="/user/training" element={<About />} />
-          <Route path="/user/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="/user/home" element={<Home />} />
+                    <Route path="/user/news" element={<News />} />
+                    <Route path="/user/course" element={<Courses />} />
+                    <Route path="/user/about" element={<About />} />
+                    <Route path="/user/contact" element={<Contact />} />
+                    <Route path="/user/login" element={<Login />} />
 
-          <Route
-            path="/superadmin/users"
-            element={
-              <AdminRoute>
-                <Users />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/dashboard"
-            element={
-              <AdminRoute>
-                <AdminDashboard />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/course"
-            element={
-              <AdminRoute>
-                <AdminCourse />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/course/create"
-            element={
-              <AdminRoute>
-                <CourseCreate />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/course/edit/:courseId"
-            element={
-              <AdminRoute>
-                <CourseEdit />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/course/:id"
-            element={
-              <AdminRoute>
-                <CourseDetail />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/training"
-            element={
-              <AdminRoute>
-                <AdminTraining />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/workshop"
-            element={
-              <AdminRoute>
-                <AdminWorkshop />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/event"
-            element={
-              <AdminRoute>
-                <AdminEvent />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/major"
-            element={
-              <AdminRoute>
-                <AdminMajor />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/partner"
-            element={
-              <AdminRoute>
-                <AdminPartner />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/feedback"
-            element={
-              <AdminRoute>
-                <Feedback />
-              </AdminRoute>
-            }
-          />
-          <Route
-            path="/admin/profile"
-            element={
-              <AdminRoute>
-                <AdminProfile />
-              </AdminRoute>
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+                    {/* SuperAdmin and Admin Routes */}
+                    <Route
+                        path="/superadmin/users"
+                        element={
+                            <AdminRoute>
+                                <Users />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/dashboard"
+                        element={
+                            <AdminRoute>
+                                <AdminDashboard />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/course"
+                        element={
+                            <AdminRoute>
+                                <AdminCourse />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/course/create"
+                        element={
+                            <AdminRoute>
+                                <CourseCreate />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/course/edit/:courseId"
+                        element={
+                            <AdminRoute>
+                                <CourseEdit />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/course/:id"
+                        element={
+                            <AdminRoute>
+                                <CourseDetail />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/training"
+                        element={
+                            <AdminRoute>
+                                <AdminTraining />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/workshop"
+                        element={
+                            <AdminRoute>
+                                <AdminWorkshop />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/event"
+                        element={
+                            <AdminRoute>
+                                <AdminEvent />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/major"
+                        element={
+                            <AdminRoute>
+                                <AdminMajor />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/partner"
+                        element={
+                            <AdminRoute>
+                                <AdminPartner />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/feedback"
+                        element={
+                            <AdminRoute>
+                                <Feedback />
+                            </AdminRoute>
+                        }
+                    />
+                    <Route
+                        path="/admin/profile"
+                        element={
+                            <AdminRoute>
+                                <AdminProfile />
+                            </AdminRoute>
+                        }
+                    />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
